4c0e94330c51ad245ae8b026402d5912f8e8c07c
tdoublee2002 <tdoublee2002@gmail.com>
2024-11-12 08:04:45 +0700

    renamed score display

--- src/components/ScoreDisplay.tsx
+++ src/components/ScoreDisplay.tsx
@@ -11,7 +11,7 @@ const ScoreDisplay: React.FC<ScoreDisplayProps> = ({ xWins, ties, oWins }) => {
     <div className="grid grid-cols-3 gap-4 w-full max-w-[460px] mx-auto mt-6 md:mt-5">
       {/* X (P2) Score */}
       <div className="flex flex-col items-center bg-[#31C3BD] text-[#1F3641] font-bold rounded-lg py-2 px-4">
-        <p className="text-xs md:text-xl">X (P2)</p>
+        <p className="text-xs md:text-xl">X WIN</p>
         <p className="text-lg md:text-2xl">{xWins}</p>
       </div>
 
@@ -23,7 +23,7 @@ const ScoreDisplay: React.FC<ScoreDisplayProps> = ({ xWins, ties, oWins }) => {
 
       {/* O (P1) Score */}
       <div className="flex flex-col items-center bg-[#F2B137] text-[#1F3641] font-bold rounded-lg py-2 px-4">
-        <p className="text-xs md:text-xl">O (P1)</p>
+        <p className="text-xs md:text-xl">O WIN</p>
         <p className="text-lg md:text-2xl">{oWins}</p>
       </div>
     </div>
